3dc0074ea446028705306d50a187ccdf25fe301d
polizz <firebomb@gmail.com>
2017-12-13 17:06:16 -0600

    Initial

---
+++ tfdlookup/content/code.js
@@ -0,0 +1,31 @@
+if (!com) var com = {};
+if (!com.polizz) com.polizz = {};
+if (!com.polizz.tfdlookup) com.polizz.tfdlookup = {};
+    
+ com.polizz.tfdlookup = {
+
+        onLoad : function() {
+            com.polizz.tfdlookup.init();
+        },
+        
+        init : function ()  {
+            var contextMenu = document.getElementById("contentAreaContextMenu"); 
+            
+            if (contextMenu)  
+                contextMenu.addEventListener("popupshowing", function() { com.polizz.tfdlookup.showHideMenu(); }, false);  
+        } ,
+          
+        showHideMenu : function ()  {
+          var show = document.getElementById("tfdLookupMenuitem"); 
+          var selectedtext = content.getSelection();
+          
+          show.label = 'Search TFD.com for "' + selectedtext + '"';
+          show.hidden = (selectedtext == ""); 
+        } ,
+        
+        SearchTFD : function() {    
+           gBrowser.addTab("http://thefreedictionary.com/" + content.getSelection());  
+        }
+    };
+
+window.addEventListener("load", function() { com.polizz.tfdlookup.onLoad(); }, false);
