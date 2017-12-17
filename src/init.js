845af917da102a36bbece1a9e83291ea1dba646f
polizz <firebomb@gmail.com>
2017-12-17 13:37:56 -0600

    Clean out console logs

--- src/init.js
+++ src/init.js
@@ -5,24 +5,18 @@ const setup = () => {
   const tfdUrl = 'http://thefreedictionary.com/'
   const menuId = 'tfdLookupMenuitem'
   
-  console.log('browser', browser)
-
   const getActiveTab = () => {
     return browser.tabs.query({
       active: true,
       currentWindow: true,
     })
       .then(tabs => {
-        console.log('tabss', tabs)
-
         return tabs[0]
       })
       .catch(err => console.log('error', err))
   }
 
   browser.tabs.onActivated.addListener(({ tabId }) => {
-    console.log('onActivated.addListener', 'tabId', tabId)
-
     browser.tabs.sendMessage(tabId, { sync: true })
       .then(res => console.log(res))
       .catch(err => console.log(err))
@@ -38,14 +32,10 @@ const setup = () => {
   
     resp(true)
   })
-  
+
   const onClickHandler = ({ selectionText: word }) => {
-    console.log(`open new tab for ${word}`)
-  
     getActiveTab()
       .then(activeTab => {
-        console.log('idx', activeTab.index)
-  
         browser.tabs.create({
           active: true,
           url: `${tfdUrl}${word}`,
