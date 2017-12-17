cfd49b77e5710cd4156b0c0f4152cf11e502b571
polizz <firebomb@gmail.com>
2017-12-17 13:27:41 -0600

    Restructure project, build dynamic manifests

--- buildManifest.js
+++ buildManifest.js
@@ -1,11 +1,30 @@
-// import fs from 'fs'
+import fs from 'fs'
 
-// const writeManifest = async () => {
-//   const file = await fs.readFile('./manifest.json')
+const readJSON = path =>
+  new Promise((res, rej) => {
+    fs.readFile(path, 'utf8', (err, data) =>
+      err ? rej(err) : res(JSON.parse(data)))
+  })
 
-//   const json = JSON.parse(file)
+const writeJSON = (path, obj) =>
+  new Promise((res, rej) => {
+    fs.writeFile(path, JSON.stringify(obj, null, 2), err =>
+      err ? rej(err) : res(true))
+  })
 
-//   json
-// }
+const readManifest = async () => {
+  const mainManifest = await readJSON('manifest/manifest.main.json')
+  const chromeManifest = await readJSON('manifest/manifest.chrome.json')
+  const mozManifest = await readJSON('manifest/manifest.moz.json')
 
-// export default writeManifest()
+  return [ mainManifest, chromeManifest, mozManifest ]
+}
+
+const writeManifests = () =>
+  readManifest().then(([ main, chrome, moz ]) =>
+    Promise.all([
+      writeJSON('dist/chrome/manifest.json', { ...main, ...chrome }),
+      writeJSON('dist/mozilla/manifest.json', { ...main, ...moz }),
+    ]))
+
+export default Promise.all([ writeManifests() ])
